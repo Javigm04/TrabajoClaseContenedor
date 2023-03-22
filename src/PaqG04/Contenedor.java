c1952f6eb15c6caa2ed0bd7707af02f79508300b
Jesús Barrajón <JesusFernando.Barrajon@alu.uclm.es>
2023-03-22 18:57:23 +0100

    control de nulos en setters

--- src/PaqG04/Contenedor.java
+++ src/PaqG04/Contenedor.java
@@ -13,36 +13,29 @@ public class Contenedor {
     public void setId(int id) {
         this.id = id;
     }
-
     public int getId() {
         return id;
     }
-
     public void setPeso(int peso) {
         this.peso = peso;
     }
-
     public int getPeso() {
         return peso;
     }
-
-    public void setPais(String pais) {
-        this.pais = pais;
+    public void setPais (String pais) {
+        if (pais != null) this.pais = pais;
     }
-
     public String getPais() {
         return pais;
     }
-
-    public void setAduanas(boolean aduanas) {
-        this.aduanas=aduanas;
+    public boolean isAduanas() {
+        return aduanas;
     }
-
     public void setPrioridad(int prioridad) {
         this.prioridad = prioridad;
     }
-
     public int getPrioridad() {
         return prioridad;
     }
+
 }
