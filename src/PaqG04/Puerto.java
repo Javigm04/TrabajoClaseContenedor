79e4f44da50660d86691eb8f7260339db4255ce2
Usuario <Javier.Garcia127@alu.uclm.es>
2023-05-10 18:50:28 +0200

    Commit puerto inicial

--- src/PaqG04/Puerto.java
+++ src/PaqG04/Puerto.java
@@ -1,5 +1,24 @@
 package PaqG04;
 
-public class Puerto {
+import java.io.Serializable;
+
+public class Puerto implements Serializable {
+    private Hub[] hubs;
+
+    public Puerto(){
+        //Inicializo los hubs
+        hubs=new Hub[3];
+        for(int i=0;i<3;i++){
+            hubs[i]=new Hub(10,12);
+        }
+    }
+
+    public String toString(){
+        String resultado="";
+        for(int i=0;i<3;i++){
+            resultado+=hubs[i].toString()+"\n";
+        }
+        return resultado;
+    }
 
 }
